cea50d18fa7e74c08a907be582f2451c2e805d9d
whg <whg@makrosolutions.com>
2024-01-16 10:56:21 +0100

    pagination added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PhotoList from './pages/Photolist';
-//import './App.css'; // Importing the CSS file
+import Pagination from './pages/Pagination';
 import './App.css';
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [photosPerPage] = useState(10); // Change this to display more or less items per page
 
   useEffect(() => {
     const fetchPhotos = async () => {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/photos?_limit=10'); // Fetching only 10 photos for example
+      const response = await axios.get('https://jsonplaceholder.typicode.com/photos?_limit=100'); // Fetching only 10 photos for example
       setPhotos(response.data);
     };
 
     fetchPhotos();
   }, []);
 
+  // Get current posts
+  const indexOfLastPhoto = currentPage * photosPerPage;
+  const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage;
+  const currentPhotos = photos.slice(indexOfFirstPhoto, indexOfLastPhoto);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
-    <div>
+    <div className='app'>
       <h1>Photo Gallery</h1>
-      <PhotoList photos={photos} />
+      <PhotoList photos={currentPhotos} />
+      <Pagination
+        itemsPerPage={photosPerPage}
+        totalItems={photos.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
