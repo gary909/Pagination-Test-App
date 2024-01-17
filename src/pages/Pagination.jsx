26a75f80f9e610f35846571c635bca3032fd197e
whg <whg@makrosolutions.com>
2024-01-17 08:04:04 +0100

    added page arrows with first last page disabled

--- src/pages/Pagination.jsx
+++ src/pages/Pagination.jsx
@@ -1,24 +1,34 @@
 const Pagination = ({ itemsPerPage, totalItems, paginate, currentPage }) => {
-    const pageNumbers = [];
-  
-    for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-      pageNumbers.push(i);
-    }
-  
-    return (
-      <nav>
-        <ul className='pagination'>
-          {pageNumbers.map(number => (
-            <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
-              <a onClick={() => paginate(number)} href='!#' className='page-link'>
-                {number}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    );
-  };
-  
-  export default Pagination;
-  
+  const pageNumbers = [];
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a onClick={() => currentPage > 1 && paginate(currentPage - 1)} href='!#' className='page-link'>
+            &laquo;
+          </a>
+        </li>
+        {pageNumbers.map(number => (
+          <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+        <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+          <a onClick={() => currentPage < totalPages && paginate(currentPage + 1)} href='!#' className='page-link'>
+            &raquo;
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
